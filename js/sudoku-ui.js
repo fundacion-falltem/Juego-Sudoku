6738cbdf857036e4d6da8af56a56aa0525746512
MrMatiasOs <m0256ai@gmail.com>
2026-01-06 16:10:45 +0200

    switch to tailwind base layout for sudoku

--- js/sudoku-ui.js
+++ js/sudoku-ui.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < 81; i++) {
     const cell = document.createElement("div");
-    cell.className = "cell";
+    cell.className =
+      "aspect-square bg-white border-2 border-neutral-300 text-lg flex items-center justify-center cursor-pointer";
     cell.textContent = "";
     board.appendChild(cell);
   }
